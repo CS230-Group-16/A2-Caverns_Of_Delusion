f8f0ac175e584eac6cc178f3a8d92c428558bfde
Bartosz <73791198+bkubica1@users.noreply.github.com>
2020-11-10 15:16:41 +0000

    Delete test2.java

--- test2.java
+++
@@ -1,7 +0,0 @@
-public class test2 {
-    public static void main (String [] args){
-        System.out.println("Hello");
-        System.out.println("World");
-        System.out.println("!");
-    }
-}
