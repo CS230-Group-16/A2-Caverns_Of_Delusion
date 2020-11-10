8aae0dafcbea20a8101a5ef04dcd9a66d7adc080
bkubica1 <976006@swansea.ac.uk>
2020-11-10 15:14:23 +0000

    another line added

--- test2.java
+++ test2.java
@@ -2,5 +2,6 @@ public class test2 {
     public static void main (String [] args){
         System.out.println("Hello");
         System.out.println("World");
+        System.out.println("!");
     }
 }
