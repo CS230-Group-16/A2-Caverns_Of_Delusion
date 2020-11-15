fec0144880ae7e1b7d0d9d57058873b2200b26d9
Michelle Bhaskaran <73836629+MichelleBhaskaran@users.noreply.github.com>
2020-11-15 22:42:44 +0000

    Leaderboard.java Class

---
+++ Leaderboard.java
@@ -0,0 +1,40 @@
+/**
+ * The Leaderboard class creates a leaderboard for the players
+ * @author Michelle Bhaskaran & Chloe Thomas
+ * @version 0.1
+ */
+
+public class Leaderboard {
+
+    /**
+     * Constructor used to make leaderboard
+     * @param leaderboardFile the leaderboard
+     */
+    public Leaderboard(String leaderboardFile) {
+        this.leaderboardFile = leaderboardFile;
+    }
+
+    /**
+     * Inserts score to the player
+     * @param player the player's profile
+     * @param score the player's score
+     */
+    public insertScore(Player player, int score) {
+
+    }
+
+    /**
+     * Sorts the leaderboard from highest to lowest score
+     */
+    private void sort() {
+
+    }
+
+
+    /**
+     * Updates the file
+     */
+    public void updateFile() {
+
+    }
+}
