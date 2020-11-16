33223f6a3006efd52d094983cbf5c4f4144aa410
bkubica1 <976006@swansea.ac.uk>
2020-11-16 13:34:09 +0000

    formated spacing

--- Board.java
+++ Board.java
@@ -6,14 +6,15 @@
  * @version 1.0
  */
 public class Board {
-	private static int[] player1Location;
-	private static int[] player2Location;
-	private static int[] player3Location;
-	private static int[] player4Location;
-	private static int width;
-	private static int height;
-	
-	/*
+
+    private static int[] player1Location;
+    private static int[] player2Location;
+    private static int[] player3Location;
+    private static int[] player4Location;
+    private static int width;
+    private static int height;
+
+    /*
 	 * Create a gameboard with every players' location, 
 	 * gameboard height&width and all non-moving tiles, orientation
 	 * of tiles and their locations
@@ -26,139 +27,155 @@ public class Board {
 	 * @param fixedTiles all non-moving tiles
 	 * @param tileRotation orientation of tile(0 degrees, 90 degrees, ...)
 	 * @param tileLocation location of tile 
-	 */
-	Board(int[] player1Location, int[] player2Location, int[] player3Location, int[] player4Location,
-			int width, int height, String[] fixedTiles, int[] tileRotation, int[] tileLocation){
-		this.player1Location = player1Location;
-		this.player2Location = player2Location;
-		this.player3Location = player3Location;
-		this.player4Location = player4Location;
-		this.width = width;
-		this.height = height;
-	}
-	
-	/*
+     */
+    Board(int[] player1Location, int[] player2Location, int[] player3Location, int[] player4Location,
+            int width, int height, String[] fixedTiles, int[] tileRotation, int[] tileLocation) {
+        this.player1Location = player1Location;
+        this.player2Location = player2Location;
+        this.player3Location = player3Location;
+        this.player4Location = player4Location;
+        this.width = width;
+        this.height = height;
+    }
+
+    /*
 	 * gets the player's location on gameboard
 	 * @param player identification of player by number
 	 * @return player's location with in [x,y] format
-	 */
-	int[] getPlayerLocation(int player) {
-		if(player == 1) {return player1Location;}
-		if(player == 2) {return player2Location;}
-		if(player == 3) {return player3Location;}
-		if(player == 4) {return player4Location;}
-		
-		return null;
-	}
-	
-	/*
+     */
+    int[] getPlayerLocation(int player) {
+        if (player == 1) {
+            return player1Location;
+        }
+        if (player == 2) {
+            return player2Location;
+        }
+        if (player == 3) {
+            return player3Location;
+        }
+        if (player == 4) {
+            return player4Location;
+        }
+
+        return null;
+    }
+
+    /*
 	 * updates/sets a new location for the player on the gameboard
 	 * @param player identification of player by number
 	 * @param newLocation the new [x,y] location of player
-	 */
-	void updatePlayerLocation(int player, int[] newLocation) {
-		if(player == 1) {player1Location = newLocation;}
-		if(player == 2) {player2Location = newLocation;}
-		if(player == 3) {player3Location = newLocation;}
-		if(player == 4) {player4Location = newLocation;}
-	}
-	
-	/*
+     */
+    void updatePlayerLocation(int player, int[] newLocation) {
+        if (player == 1) {
+            player1Location = newLocation;
+        }
+        if (player == 2) {
+            player2Location = newLocation;
+        }
+        if (player == 3) {
+            player3Location = newLocation;
+        }
+        if (player == 4) {
+            player4Location = newLocation;
+        }
+    }
+
+    /*
 	 * inserts tile on gameboard&pushes all tiles along 
 	 * @param tile FloorTile to be inserted
 	 * @param row can only be inserted if true
 	 * @param positionNum where the tile should be inserted(column)
 	 * @param rotation orientation of the tile(0 degrees, 90 degrees, ..)
-	 */
-	void insertTile(FloorTile tile, Boolean row, int positionNum, int rotation) {
-		;
-	}
-	
-	/* creates a floor tile(tile with no special actions, only 
+     */
+    void insertTile(FloorTile tile, Boolean row, int positionNum, int rotation) {
+        ;
+    }
+
+    /* creates a floor tile(tile with no special actions, only 
 	 * 						different shapes)
 	 * @param type the type of the tile
 	 * @param rotation orientation of the tile(0 degrees, 90 degrees, ..)
 	 * @return FloorTile tile
-	 */
-	private FloorTile createTile(String type, int rotation) {
-		;
-	}
-	
-	/*
+     */
+    private FloorTile createTile(String type, int rotation) {
+        ;
+    }
+
+    /*
 	 * checks if player is out of the game board
-	 */
-	public void pushedOut() {
-		;
-	}
-	
-	/*
+     */
+    public void pushedOut() {
+        ;
+    }
+
+    /*
 	 * checks if a tile is 'frozen'(frozen tiles
 	 * 						act as FloorTile[fixed tiles]
 	 * 						until ice melts)
 	 * @param positionNum the position of the tile
 	 * @return whether the tile is frozen or not
-	 */
-	private Boolean isFrozen(int positionNum) {
-		return false;
-	}
-	
-	/*
+     */
+    private Boolean isFrozen(int positionNum) {
+        return false;
+    }
+
+    /*
 	 * checks if a tile is 'engulfed'(fire on
 	 * 						surrounding tiles, cannot
 	 * 						be moved onto if on fire) 
 	 * @return whether the tiles are engulfed or not  					    
-	 */
-	private Boolean isEngulfed() {
-		return false;
-	}
-	
-	/*
+     */
+    private Boolean isEngulfed() {
+        return false;
+    }
+
+    /*
 	 * checks if player has reached finish
 	 * @return whether player is finished or not
-	 */
-	Boolean reachedGoal() {
-		return false;
-	}
-	
-	/*
+     */
+    Boolean reachedGoal() {
+        return false;
+    }
+
+    /*
 	 * checks if tile can be used to make a path
 	 * @param tile the tile to be checked
-	 */
-	void checkPathway(Tile tile) {
-		;
-	}
-	
-	/*
+     */
+    void checkPathway(Tile tile) {
+        ;
+    }
+
+    /*
 	 * when a player's turn, signifies their action/'move'
 	 * @param playerNum
-	 */
-	void move(int playerNum) {
-		;
-	}
-	
-	/*
+     */
+    void move(int playerNum) {
+        ;
+    }
+
+    /*
 	 * moves the player forwards/backwards depending on return
 	 * @param move if true player moves forward, otherwise backwards
 	 * @param playerNum player's number
 	 * @param finalLocation player's goal location
-	 */
-	void movePlayer(Boolean move, int playerNum, int[] finalLocation) {
-		;
-	}
-	
-	/*
+     */
+    void movePlayer(Boolean move, int playerNum, int[] finalLocation) {
+        ;
+    }
+
+    /*
 	 * saves board constructor to a .txt file
-	 */
-	void saveBoardToFile() {
-		;
-	}
-	
-	/*
+     */
+    void saveBoardToFile() {
+        ;
+    }
+
+    /*
 	 * places tile on gameboard(used when constructing board)
 	 * @param tile tile to be placed
 	 * @param location where the tile should be placed
-	 */
-	void placeTile(FloorTile tile, int[] location) {
-		;
-	}
+     */
+    void placeTile(FloorTile tile, int[] location) {
+        ;
+    }
 }
