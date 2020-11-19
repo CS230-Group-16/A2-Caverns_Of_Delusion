7f52f6081937b503669f6e3de0beee83a98dbd04
bkubica1 <976006@swansea.ac.uk>
2020-11-19 18:23:47 +0000

    split tile and floor tile classes

---
+++ FloorTile.java
@@ -0,0 +1,31 @@
+abstract class FloorTile extends Tile {
+    private boolean frozen;
+    private boolean engulfed;
+    private boolean occupied;
+    private String tileType;
+
+    public FloorTile(String type, String tileType) {
+        super(type);
+        this.tileType = tileType;
+    }
+
+    public void setFrozen(boolean value) {
+        this.frozen = value;
+    }
+    public void setEngulfed(boolean value) {
+        this.engulfed = value;
+    }
+    public void setOccupied(boolean value) {
+        this.occupied = value;
+    }
+
+    public boolean isFrozen() {
+        return this.frozen;
+    }
+    public boolean isEngulfed() {
+        return this.engulfed;
+    }
+    public boolean isOccupied() {
+        return this.occupied;
+    }
+}
