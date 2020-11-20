b7b0fbaa2704e512668bd4eb5ed5bee7217f31ab
Michelle Bhaskaran <73836629+MichelleBhaskaran@users.noreply.github.com>
2020-11-20 19:44:45 +0000

    updated main

--- Main.java
+++ Main.java
@@ -8,10 +8,12 @@ public class Main {
         Game g = new Game("D:/Documents/NetBeansProjects/A2-Caverns_Of_Delusion/board1.txt",new String[]{"Super_Cool_Name","grapeLord5000"});
         g.toStr();
         
-        Player p1 = new Player("Michelle", 2, 1, 4);
-		Player p2 = new Player("Chloe", 5, 3, 1);
+        Player p1 = new Player("Michelle", 2, 1);
+		Player p2 = new Player("Chloe", 5, 3);
 		
 		p1.saveProfile();
 	    p2.saveProfile();
+	    p1.updateUsername ("Blah");
+	    p1.saveProfile();
     }
 }
