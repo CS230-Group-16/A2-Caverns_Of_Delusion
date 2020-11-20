4096b31503b20a73867071d71a33c7372e6c1176
bkubica1 <976006@swansea.ac.uk>
2020-11-20 11:17:34 +0000

    testing game object

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@
 public class Main {
     
     public static void main(String [] args){
-        
+        Game g = new Game("D:/Documents/NetBeansProjects/A2-Caverns_Of_Delusion/board1.txt",new String[]{"Super_Cool_Name","grapeLord5000"});
+        g.toStr();
     }
 }
