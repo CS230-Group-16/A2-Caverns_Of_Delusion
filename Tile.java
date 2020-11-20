5e1d0428a22b07797d9627e35429d079eb04acaf
CMcDonagh01 <53273390+CMcDonagh01@users.noreply.github.com>
2020-11-20 15:22:37 +0000

    Update Tile.java
    Added JavaDoc for Tile Class and reformatted to meet coding conventions

--- Tile.java
+++ Tile.java
@@ -1,12 +1,30 @@
+/*
+ * class represents the abstract class of tile, cannot be
+ * instantiated.
+ * @author Cameron McDonagh & Maciej Buczkowski
+ * @version 1.1
+ */
+
 abstract class Tile {
+    /* The 'type; attribute is protected, meaning that it
+     * can only be access within the package, or by any
+     * subclasses */
     protected final String type;
 
-    public String getType() {
-        return this.type;
-    }
-
+    /**
+    * Constructor used to the Tile class
+    *
+    * @param type - the type of the current tile.
+    */
     public Tile (String type){
         this.type = type;
     }
-}
 
+    /**
+     * Gets the tile's type
+     * @return tile type
+     */
+    public String getType() {
+        return this.type;
+    }
+}
