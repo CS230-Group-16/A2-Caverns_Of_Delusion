016f0b208c81fc52d9c5af6dbdddadf742736bc1
CMcDonagh01 <53273390+CMcDonagh01@users.noreply.github.com>
2020-11-20 16:25:19 +0000

    Create GoalTile.java
    Added GoalTile class

---
+++ GoalTile.java
@@ -0,0 +1,17 @@
+/*
+ * class represents the floor tile of type 'T-Shape'
+ * @author Cameron McDonagh & Maciej Buczkowski
+ * @version 1.1
+ */
+
+public class GoalTile extends FloorTile  {
+
+	/**
+	 * Constructor used to make a corner tile
+	 *
+	 * @param type the type of tile (Goal)
+	 */
+	public GoalTile (String type) {
+			super(type);
+	}
+}
