baec32d812d21e96c60841270446ac05dacde92e
CMcDonagh01 <53273390+CMcDonagh01@users.noreply.github.com>
2020-11-20 18:13:41 +0000

    Update TileTester.java

--- TileTester.java
+++ TileTester.java
@@ -3,6 +3,19 @@ Added this file to easily test tiles without needing a main
 method in each class to test
 */
 
+/*
+ Tile constructor codes:
+
+ Floor Tiles:
+     - "Straight"
+     - "Corner"
+     - "TShape"
+		 - "Goal"
+
+Action Tiles:
+		 - "Movement"
+		 - "Effect"
+*/
 public class TileTester {
 	public static void main(String[] args) {
 		ActionTile t1 = new ActionTile("Freeze");
