7f349cb3c0ec4647fe9b9f0dcc1d293730d1c03c
MaciejBuczkowski <MaciejBuczkowski@users.noreply.github.com>
2020-11-22 22:19:17 +0000

    both types of action tiles, player movement effects and board effects

---
+++ EffectTile.java
@@ -0,0 +1,41 @@
+/**
+ * class represents effect tiles that effect the board
+ * @author Cameron McDonagh & Maciej Buczkowski
+ * @version 1.0
+ */
+public class EffectTile extends ActionTile {
+
+    /**
+     * Constructor to make Effect Tiles (engulf/freeze)
+     * @param effect desired tile effect
+     * @param turnDrawn turn drawn so that effects last the appropriate amount of time
+     */
+    public EffectTile(String effect, Integer turnDrawn){
+        super(effect, turnDrawn);
+    }
+
+    /**
+     * sets tile on fire
+     * @param tile tile you wish to effect
+     */
+    public static void engulf(FloorTile tile){
+        tile.setEngulfed();
+    }
+
+    /**
+     *
+     * @return effect of tile
+     */
+    public String getAction(){
+        return super.getEffect();
+    }
+
+    /**
+     * freezes tile
+     * @param tile tile you with to effect
+     */
+    public static void freeze(FloorTile tile){
+            tile.setFrozen();
+    }
+
+}
---
+++ MovementTile.java
@@ -0,0 +1,41 @@
+/**
+ * class represents the tiles effecting player movement
+ * @author Cameron McDonagh & Maciej Buczkowski
+ * @version 1.0
+ */
+public class MovementTile extends ActionTile {
+
+    public Boolean movement;
+
+    /**
+     * Construtor for making movement based action tiles
+     * @param effect desired effect (backTrack/doubleMove)
+     * @param turnDrawn
+     */
+    public MovementTile(String effect,Integer turnDrawn) {
+        super(effect, turnDrawn);
+        if (effect == "backTrack"){
+            this.movement = false; // false means go back
+        }else if (effect == "doubleMove"){
+            this.movement = true; // true means go forwards
+        }
+
+    }
+
+    /**
+     *
+     * @return the type of movement (false return = backTrack/ true return = doubleMove)
+     */
+    public Boolean getMovement() {
+        return movement;
+    }
+
+    /**
+     *
+     * @return turn the tile was drawn
+     */
+    public Integer getTurnDrawn(){
+        return super.getTurnDrawn();
+    }
+
+}
