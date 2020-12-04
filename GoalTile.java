682d5a0ee76b4826ed3ebef636bd5ea1d25e001c
bkubica1 <976006@swansea.ac.uk>
2020-12-04 17:10:33 +0000

    added second constructor

--- GoalTile.java
+++ GoalTile.java
@@ -16,6 +16,10 @@ public class GoalTile extends FloorTile {
         super.setPathways(new int[] {1,1,1,1});
     }
 
+    public GoalTile(boolean frozen, boolean engulfed, boolean fixed, boolean occupied,int rotation){
+        super("GOAL",frozen,engulfed,fixed,occupied,rotation);
+    }
+    
     /**
      * get pathways of goal tile
      * @return pathway (1 being true and 0 being false)
