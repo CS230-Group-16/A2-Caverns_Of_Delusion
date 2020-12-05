419ba35bf4ff2bc796716134c5be7b494a0b3e23
bkubica1 <976006@swansea.ac.uk>
2020-12-05 13:52:43 +0000

    fixed image

--- ProfileEdit.java
+++ ProfileEdit.java
@@ -5,6 +5,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
@@ -132,7 +133,7 @@ public class ProfileEdit extends Application {
 
         VBox root = new VBox();
         
-        Image image = new Image(DIRECTORY + "images/background_image.jpg");
+        Image image = new Image(new FileInputStream(DIRECTORY + "images/background_image.jpg"));
         BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
         BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         Background backgroundPicture = new Background(backgroundImage);
