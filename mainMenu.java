ea943d9c52520e7a2e9725731580754d96730166
MaciejBuczkowski <MaciejBuczkowski@users.noreply.github.com>
2020-12-06 16:06:29 +0000

    Update mainMenu.java

--- mainMenu.java
+++ mainMenu.java
@@ -1,28 +1,31 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.TextFlow;
-import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 
-import java.io.IOException;
+import java.io.*;
+
+import javafx.application.Application;
+        import javafx.event.ActionEvent;
+        import javafx.event.EventHandler;
+        import javafx.fxml.FXML;
+        import javafx.scene.Node;
+        import javafx.scene.Parent;
+        import javafx.scene.control.TextArea;
+        import javafx.scene.layout.AnchorPane;
+        import javafx.scene.layout.Pane;
+        import javafx.scene.text.TextFlow;
+        import javafx.stage.Stage;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Scene;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.Label;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
+        import javafx.scene.layout.Background;
+        import javafx.scene.layout.BackgroundImage;
+        import javafx.scene.layout.BackgroundPosition;
+        import javafx.scene.layout.BackgroundRepeat;
+        import javafx.scene.layout.BackgroundSize;
 
 /**
  * class represents main menu
@@ -31,16 +34,24 @@ import javafx.scene.layout.BackgroundSize;
  */
 public class mainMenu extends Application{
 
-	/**
-	 * creates the main menu scene
-	 * @param stage To create the stage
-	 */  
-    
+    @FXML
+    public Label msgOfTheDay = new Label();
+
+
+    /**
+     * creates the main menu scene
+     * @param stage To create the stage
+     */
+
     public void start(Stage stage) throws FileNotFoundException {
-        
+
         try {
             Parent root = (Parent) FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
-            
+
+            msgOfTheDay.setText(displayMessage());
+            msgOfTheDay.setWrapText(true);
+            msgOfTheDay.setVisible(true);
+
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
@@ -50,18 +61,16 @@ public class mainMenu extends Application{
             e.printStackTrace();
         }
     }
-    
+
     public static void main(String [] args) {
         launch(args);
     }
 
-    @FXML
-    private Label msgOfTheDay;
-    
+
     /**
      * handles the playing
-     * @param an event to handle the playing
-     * @throws an IOException
+     * @param  event to handle the playing
+     * @throws IOException
      */
     @FXML
     private void handlePlayAction(ActionEvent event) throws IOException {
@@ -72,7 +81,7 @@ public class mainMenu extends Application{
         gameStage.setScene(gameScene);
         gameStage.show();
     }
-    
+
     /**
      * handles the leaderboard actions
      * @param event An event to handle the leaderboard action
@@ -87,7 +96,7 @@ public class mainMenu extends Application{
             e.printStackTrace();
         }
     }
-    
+
     /**
      * Handles editing palyer's action
      * @param event An event to edit the player's action
@@ -102,7 +111,7 @@ public class mainMenu extends Application{
             e.printStackTrace();
         }
     }
-    
+
     /**
      * This allows the exit of the game
      * @param event An event to exit the game
@@ -115,5 +124,69 @@ public class mainMenu extends Application{
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Displays daily message
+     */
+    private String displayMessage() {
+        String message = getMessage("http://cswebcat.swansea.ac.uk/message?solution="
+                + decodeMessage(getMessage("http://cswebcat.swansea.ac.uk/puzzle")));
+
+        return message;
+    }
+
+    /**
+     * Decodes the message as per the instructions on the web site
+     * @param msg message to decode
+     * @return decoded message
+     */
+    private String decodeMessage(String msg) {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String solution = "CS-230";
+        int alphabetLength = alphabet.length();
+        boolean back = true;
+        int move = 0;
+        String[] msgArr = msg.split("");
+        for (int i = 0; i < msg.length(); i++) {
+            if (back) {
+                move = alphabet.indexOf(msgArr[i]) - (i + 1);
+                if (move < 0) {
+                    move = alphabetLength - (move * -1);
+                }
+                solution += alphabet.charAt(move);
+                back = false;
+            } else {
+                move = alphabet.indexOf(msgArr[i]) + (i + 1);
+                if (move >= alphabetLength) {
+                    move = (move - alphabetLength);
+                }
+                solution += alphabet.charAt(move);
+                back = true;
+            }
+        }
+        solution = solution + Integer.toString(solution.length());
+        return solution;
+    }
+
+    /**
+     * Gets the message from a specific URL
+     * @param stringURL URL to get the message from
+     * @return the message from the website
+     */
+    private String getMessage(String stringURL) {
+        String msg = "";
+        try {
+            URL url = new URL(stringURL);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            msg = in.readLine();
+            in.close();
+        } catch (Exception e) {
+            System.err.println("Something went wrong");
+            e.printStackTrace();
+        }
+        return msg;
     }
 }
